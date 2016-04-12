a6903ba6c95448d90a7626d099f373a522c2b35d
Rich Schumacher <rich.schu@gmail.com>
2016-04-12 15:46:24 -0400

    Add some extra fields to metadata.rb:
    * source_url
    * issues_url
    * chef_version

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,10 @@ license          'MIT'
 description      'Installs and configures Zipkin, a distributed tracing system.'
 long_description 'Installs and configures Zipkin, a distributed tracing system.'
 version          '0.1.0'
+source_url       'https://github.com/curalate/chef-zipkin' if respond_to?(:source_url)
+issues_url       'https://github.com/curalate/chef-zipkin/issues' if respond_to?(:issues_url)
+
+chef_version     '>= 11.0' if respond_to?(:chef_version)
 
 supports 'ubuntu'
 
